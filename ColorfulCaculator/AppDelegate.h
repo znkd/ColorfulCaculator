264e2a4e415ce632a038fa6a5d9740d09efb1d7b
zhangnand <zhangnand@yonyou.com>
2012-11-04 12:23:38 +0800

    commit

---
+++ ColorfulCaculator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ColorfulCaculator
+//
+//  Created by ufida on 12-11-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ColorfulCaculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ColorfulCaculator
+//
+//  Created by ufida on 12-11-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
